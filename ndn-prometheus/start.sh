02beeec6f061c63d1861b266923944b1ddf42ba4
Sean Liao <seankhliao@gmail.com>
2020-01-20 02:23:49 +0100

    cleanup

--- ndn-prometheus/start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-/bin/prometheus \
-    --config.file=/etc/prometheus/prometheus.yml \
-    --storage.tsdb.path=/prometheus \
-    --web.console.libraries=/usr/share/prometheus/console_libraries \
-    --web.console.templates=/usr/share/prometheus/consoles 2>&1 &
-
-/bin/watcher 2>&1
--- ndn-prometheus/watcher/go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/seankhliao/uva-rp1/ndn-prometheus/watcher
-
-go 1.14
